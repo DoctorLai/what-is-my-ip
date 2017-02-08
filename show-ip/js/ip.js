0640e55f4282b71fe7a83675648a203c42f04820
SteakOverCooked <Oh@SteakOverCooked.com>
2017-02-08 19:34:09 +0000

    comments

--- show-ip/js/ip.js
+++ show-ip/js/ip.js
@@ -1,3 +1,18 @@
+/*
+what-is-my-ip
+Chrome Extension to show Both Local and Public IP Addresses. This chrome extension shows the external and internal IP addresses when you click the extension icon.
+
+Chrome Extension URL
+https://chrome.google.com/webstore/detail/show-my-ip-addresses-exte/opljiobgnagdjikipnagigiacllolpaj
+
+Web Tool
+https://helloacm.com/what-is-my-ip/
+
+Buy me a Coffee
+https://www.paypal.me/doctorlai/5
+bitcoin:1J88t5UAgKBHhMgzkyH9bpY5mPdCYAe5XQ
+*/
+
 function getChromeVersion() {     
   var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
   return raw ? parseInt(raw[2], 10) : false;
